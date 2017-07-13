03f3a2e4a87785d80fde76578709f119100d1ea8
jiwei <jiwei3778@bjsasc.com>
2017-07-13 14:56:38 +0800

    更新生产d.ts 代码

---
+++ index.js
@@ -0,0 +1,20 @@
+var fs = require('fs');
+var CodeGen = require('swagger-js-codegen').CodeGen;
+
+var file = 'test/user.json';
+var swagger = JSON.parse(fs.readFileSync(file, 'UTF-8'));
+//var tsSourceCode = CodeGen.getTypescriptCode({ className: 'Test', swagger: swagger, imports: ['../../typings/tsd.d.ts'] });
+var tsSourceCode =CodeGen.getCustomCode({
+    moduleName: 'USERAPI',
+    className: 'USERAPI',
+    swagger: swagger,
+    imports: ["request","bluebird","node","http"] ,
+    lint:false,
+    template:{
+        class:fs.readFileSync('./template/tsd.mustache', 'utf-8'),
+        method:"",
+        type:fs.readFileSync('./template/type.mustache', 'utf-8')
+    }
+})
+console.log(tsSourceCode);
+fs.writeFileSync("user.d.ts",tsSourceCode,"UTF-8");
