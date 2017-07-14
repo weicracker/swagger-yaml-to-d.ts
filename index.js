f55e707bb147c7df05d092d6d16dec289229600a
jiwei <jiwei3778@bjsasc.com>
2017-07-14 15:32:37 +0800

    更新根据http 生成文件

--- index.js
+++ index.js
@@ -1,23 +1,39 @@
-var fs = require('fs');
-var CodeGen = require('swagger-js-codegen').CodeGen;
-const args = require('minimist')(process.argv.slice(2));
-console.log(args)
-var file = args._[1];
-let filename = args._[0];
-var swagger = JSON.parse(fs.readFileSync(file, 'UTF-8'));
-var tsSourceCode =CodeGen.getCustomCode({
-    moduleName: filename,
-    className: filename,
-    swagger: swagger,
-    imports: ["request","bluebird","node","http"] ,
-    lint:false,
-    template:{
-        class:fs.readFileSync('./template/tsd.mustache', 'utf-8'),
-        method:"",
-        type:fs.readFileSync('./template/type.mustache', 'utf-8')
-    }
-})
+var fsextra = require('fs-extra');
+var request = require("request");
+let httpURL = {
+    user: "http://api.bjsasc.com/asp/user/v2/api-docs?group=asp"
+}
 
-let filePath = `${__dirname}/${filename}.d.ts`;
-fs.writeFileSync(filePath,tsSourceCode,"UTF-8");
-process.exit(0);
+var CodeGen = require('swagger-js-codegen').CodeGen;
+let i=0;
+for( let key in httpURL){
+    request.get(httpURL[key],(res,body)=>{
+        let swagger = JSON.parse(body.body);
+        let filename = key;
+        let source = useHttp(swagger,filename);
+        let filePath = `${__dirname}/${key}/api.d.ts`;
+        fsextra.outputFileSync(filePath, tsSourceCode, "UTF-8");
+        console.log(`已完成${i++}个任务，当前正在执行${key}模块,共计${Object.keys(httpURL).length}`)
+    })
+    
+    if(Object.keys(httpURL).length==i){
+        process.exit(0);
+    }
+}
+
+
+function useHttp(swagger,filename) {
+    return tsSourceCode = CodeGen.getCustomCode({
+        moduleName: filename,
+        className: filename,
+        swagger: swagger,
+        imports: ["request", "bluebird", "node", "http"],
+        lint: false,
+        template: {
+            class: fsextra.readFileSync('./template/tsd.mustache', 'utf-8'),
+            method: "",
+            type: fsextra.readFileSync('./template/type.mustache', 'utf-8')
+        }
+    })
+}
+
